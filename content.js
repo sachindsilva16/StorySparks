67000f5723fdbcac3aafd47b51d6e7ae2faa25e7
sachindsilva16 <sachindsilvad@gmail.com>
2023-04-30 14:11:41 +0530

    Initial Commit

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 module.exports.homeStartingContent = () => {
 
-    let content = "Sed ut perspiciatis unde omnis iste natus error sit volup velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.";
+    let content = "Sed ut perspiciatis unde omnis iste natus error sit volup velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.";
 
     return content;
 
